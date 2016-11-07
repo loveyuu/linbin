7aa541ed34329dd9f1d0fbd366ce5ba90096d1b4
linbin <1253007885@qq.com>
2016-11-07 20:48:31 +0800

    update

--- linbin/urls.py
+++ linbin/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from blog import views as blog_views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^index/', blog_views.index),
 ]

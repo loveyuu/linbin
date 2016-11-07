0fff3d4d2ae3c30d50c5970b9a60d3437dfc9dca
linbin <1253007885@qq.com>
2016-11-07 23:16:15 +0800

    添加帖子

--- blog/models.py
+++ blog/models.py
@@ -2,4 +2,15 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    create = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Post(TimeStampedModel):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
